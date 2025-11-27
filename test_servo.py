f67914f626d05ebbf041c322448d5f8a59292e03
liyin <lydia0492302209@gmail.com>
2025-11-27 14:08:18 +0800

    feat:test servo

--- test_servo.py
+++ test_servo.py
@@ -1,14 +1,10 @@
-import time
+import time 
 from adafruit_servokit import ServoKit
 
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=8)
-
-kit.servo[0].angle = 180
-kit.continuous_servo[1].throttle = 1
-time.sleep(1)
-kit.continuous_servo[1].throttle = -1
-time.sleep(1)
-kit.servo[0].angle = 0
-kit.continuous_servo[1].throttle = 0
+kit = ServoKit(channels=16)
+servo = 0
+while True:
+    a=input("Enter angle (0-180): ")
+    kit.servo[0].angle = int(a)
